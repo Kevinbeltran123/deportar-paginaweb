8f7ce3cc2b3dab31e4af21bf5d54bdf1894edad3
KevinBel <kevin.beltran@estudiantesunibague.edu.co>
2025-10-05 08:25:47 -0500

    Eliminar componentes de clientes obsoletos

--- deportur-frontend/src/components/clientes/index.js
+++ deportur-frontend/src/components/clientes/index.js
@@ -1,6 +1,4 @@
-export { ListaClientes } from './ListaClientes';
 export { ListaClientesV2 } from './ListaClientesV2';
-export { FormularioCliente } from './FormularioCliente';
 export { FormularioClienteV2 } from './FormularioClienteV2';
 export { DetalleCliente } from './DetalleCliente';
 export { BuscarCliente } from './BuscarCliente';
